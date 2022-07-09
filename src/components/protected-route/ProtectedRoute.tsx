f227160f008e9560583f50ea12d132894e3527d8
Your Name <you@example.com>
2022-07-09 03:27:06 +0300

    to review

--- src/components/protected-route/ProtectedRoute.tsx
+++ src/components/protected-route/ProtectedRoute.tsx
@@ -21,8 +21,6 @@ const ProtectedRoute: FC<TProtectedRoute> = ({ children, ...rest }) => {
         init();
     }, []);
 
-    console.log(user);
-
     if (!isUserLoaded) {
         return null;
     }
