dbe0ac4c8baa139922ab6efaeb99a8d549c138df
Your Name <you@example.com>
2022-07-11 14:51:52 +0300

    fix cols

--- src/components/feed-order-info/FeedOrderInfo.tsx
+++ src/components/feed-order-info/FeedOrderInfo.tsx
@@ -8,33 +8,46 @@ const FeedOrderInfo: FC = (): ReactElement => {
     const doneOrderNumbers = orders.filter(e => e.status === "done").map(order => order.number);
     const pendingOrderNumbers = orders.filter(e => e.status === "pending").map(order => order.number);
 
+    const renderOrderNumbers = ( arr: number[], done = false ) => {
+        const chunkSize = 5;
+        const tempArr = [];
+        for (let i = 0; i < arr.length; i += chunkSize) {
+            tempArr.push(arr.slice(i, i + chunkSize))
+        }
+
+        return (
+            tempArr.slice(0, 2).map(( group, index ) =>
+                <div key={ index } className={ done ? styles.done : ""}>
+                    { group.map(( number, index ) =>
+                        <div key={ index }
+                             className={ `text text_type_digits-default ${ styles.number }` }>{ number }</div>
+                    ) }
+                </div>
+            )
+        )
+    }
+
     return (
         <>
-            <div className={`${ styles.containerInner } mt-15` }>
+            <div className={ `${ styles.containerInner } mt-15` }>
                 <div className={ styles.wrapper }>
                     <h2 className="text text_type_main-medium">Готовы:</h2>
                     <div className={ `${ styles.wrapperInner } mt-6` }>
-                        { doneOrderNumbers.slice(0, 10).map(( number, index ) =>
-                            <div key={ index }
-                                 className={ `text text_type_digits-default ${ styles.number } ${ styles.numberDone }` }>{ number }</div>
-                        ) }
+                        { renderOrderNumbers(doneOrderNumbers, true) }
                     </div>
                 </div>
                 <div className={ styles.wrapper }>
                     <h2 className="text text_type_main-medium">В работе:</h2>
                     <div className={ `${ styles.wrapperInner } mt-6` }>
-                        { pendingOrderNumbers.slice(0, 20).map(( number, index ) =>
-                            <div key={ index }
-                                 className={ `text text_type_digits-default ${ styles.number }` }>{ number }</div>
-                        ) }
+                        { renderOrderNumbers(pendingOrderNumbers) }
                     </div>
                 </div>
             </div>
-            <div className={ `${styles.wrapper} mt-15` }>
+            <div className={ `${ styles.wrapper } mt-15` }>
                 <h2 className="text text_type_main-medium">Выполнено за все время:</h2>
                 <p className={ `text text_type_digits-large ${ styles.lightNumber }` }>{ total }</p>
             </div>
-            <div className={ `${styles.wrapper} mt-15` }>
+            <div className={ `${ styles.wrapper } mt-15` }>
                 <h2 className="text text_type_main-medium">Выполнено за сегодня:</h2>
                 <p className={ `text text_type_digits-large ${ styles.lightNumber }` }>{ totalToday }</p>
             </div>
