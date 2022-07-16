085ee374e8595211da36599fc9c651e70be4e814
Your Name <you@example.com>
2022-07-16 23:56:55 +0300

    Updates

--- src/services/selectors/ingredientsSelector.ts
+++
@@ -1,7 +0,0 @@
-import { TIngredient } from "../../utils/types";
-
-
-export const getIngredients = (ingredients: Array<string>, items: readonly TIngredient[]) =>
-    ingredients
-    .map(( id ) => items
-        .find(( e ) => e._id === id)!)
